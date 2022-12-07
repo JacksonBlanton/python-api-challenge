1375f7bd1049c6fbdc8459ffd3e991e65e683fdc
JacksonBlanton <115750417+JacksonBlanton@users.noreply.github.com>
2022-12-06 23:42:04 -0600

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "09d8acc7b16ab519e916669958d4ecf7"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "42cbdab2357447118fc729d24dd52e8b"
